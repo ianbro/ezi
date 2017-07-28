9d253ecec12240ef8f78bcb85ea64e3d907e0580
Ian Kirkpatrick <ianmann56@gmail.com>
2017-07-27 23:01:13 -0400

    Increment version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup (
     name='ezi',
-    version='0.0.1',
+    version='0.0.2',
     packages=['ezi'],
     install_requires=["Django>=1.7"],
     include_package_data=True,
